428426582e2b3db8fa54ef67c76bc6a54ad23527
Giovanni1674 <106160867+Giovanni1674@users.noreply.github.com>
2023-01-28 18:20:26 +0100

    test class import
    import test class

--- src/test/test.java
+++ src/test/test.java
@@ -2,3 +2,26 @@ package test;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+//import nostri 
+
+
+//classe di test per la registrazione
+@RunWith(MockitoJUnitRunner.class)
+public class registrazioneUT
+{
+	@Mock
+	private
+}
