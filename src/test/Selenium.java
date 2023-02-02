16512c8fed159c176db612937053471afc525485
Giovanni Borrelli <101841621+GiovanniBorrelli@users.noreply.github.com>
2023-02-02 18:14:22 +0100

    Add Selenium class
    Add Selenium.java

--- src/test/Selenium.java
+++ src/test/Selenium.java
@@ -1,8 +1,15 @@
 package test;
 
-import org.openqa.selenium.chrome.ChromeDriver;
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 public class Selenium {
-	WebDriver driver = new ChromeDriver();
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.edge.driver","C:/edgedriver_win64/msedgedriver.exe");
+		WebDriver driver = new EdgeDriver();
+		driver.get("http://localhost:8080/HealthCare/RicercaMalattia.jsp");
+		
+	}
 }
