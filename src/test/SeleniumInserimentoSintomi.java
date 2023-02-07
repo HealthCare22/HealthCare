7436209787ffa7bebdb70b30dd5ad435e3d7d042
Giovanni Borrelli <101841621+GiovanniBorrelli@users.noreply.github.com>
2023-02-07 14:01:32 +0100

    Add Selenium Test Classes
    Add test classes in SeleniumInterventoSintomi.java

--- src/test/SeleniumInserimentoSintomi.java
+++ src/test/SeleniumInserimentoSintomi.java
@@ -11,26 +11,28 @@ import org.openqa.selenium.edge.EdgeDriver;
 
 public class SeleniumInserimentoSintomi {
 	
-	//Uno o più sintomi risultano incorretti
+	
 
 	/**
 	 * TCS_17.1
 	 * 1 o più sintomi inseriti risultano incorretti
 	 */
+	
+	//nessun sintomo inserito
 	@Test
-	public void inserimentoSintomi() {
+	public void inserimentoNoSintomi() {
 	System.setProperty("webdriver.edge.driver","C:/edgedriver_win64/msedgedriver.exe");
 	WebDriver driver = new EdgeDriver();
 	driver.get("http://localhost:8080/HealthCare/RicercaMalattia.jsp");
 
-	driver.findElement(By.id("sintomo1"));
-	WebElement sintomo1=driver.findElement(By.id("sintomo1"));
-	driver.findElement(By.id("sintomo2"));
-	WebElement sintomo2=driver.findElement(By.id("sintomo2"));
-	sintomo1.sendKeys("febbre");
-	sintomo2.sendKeys("aaaaaaaaaaaaaaaa");
+	WebElement perSintomi = driver.findElement(By.id("perSintomi"));
+	perSintomi.click();
+	
+	driver.findElement(By.id("Sintomo1"));
+	WebElement sintomo1=driver.findElement(By.id("Sintomo1"));
+	sintomo1.sendKeys("");
 
-	WebElement cercaButton = driver.findElement(By.id("cercaPerNomeButton"));
+	WebElement cercaButton = driver.findElement(By.id("cercaPerSintomiButton"));
 	cercaButton.click();
 
 	String actualUrl="http://localhost:8080/HealthCare/MyForm.jsp";
@@ -38,4 +40,38 @@ public class SeleniumInserimentoSintomi {
 	assertEquals(actualUrl, not(expectedUrl));
 
 	}
+
+
+	//Uno o più sintomi risultano incorretti
+	@Test
+	public void inserimentoSintomi() {
+	System.setProperty("webdriver.edge.driver","C:/edgedriver_win64/msedgedriver.exe");
+	WebDriver driver = new EdgeDriver();
+	driver.get("http://localhost:8080/HealthCare/RicercaMalattia.jsp");
+
+	WebElement perSintomi = driver.findElement(By.id("perSintomi"));
+	perSintomi.click();
+	
+
+	WebElement aggiungiSintomoButton = driver.findElement(By.id("aggiungiSintomoButton"));
+	
+	
+	driver.findElement(By.id("Sintomo1"));
+	WebElement sintomo1=driver.findElement(By.id("Sintomo1"));
+	sintomo1.sendKeys("febbre");
+	aggiungiSintomoButton.click();
+	
+	driver.findElement(By.id("Sintomo2"));
+	WebElement sintomo2=driver.findElement(By.id("Sintomo2"));
+	sintomo2.sendKeys("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+
+	WebElement cercaButton = driver.findElement(By.id("cercaPerSintomiButton"));
+	cercaButton.click();
+
+	String actualUrl="http://localhost:8080/HealthCare/MyForm.jsp";
+	String expectedUrl= driver.getCurrentUrl();
+	assertEquals(actualUrl, not(expectedUrl));
+
+	}
+	
 }
